cd89bef778737a47b30e377d5015427086f9fd3c
DarkBlakeZ <52579583+DarkBlakeZ@users.noreply.github.com>
2022-02-25 10:51:00 -0500

    Cambio de nombre TokenAuthentification

--- FinaktivaPTBackend/Authentification/TokenAuthenticationService.cs
+++ FinaktivaPTBackend/Authentification/TokenAuthenticationService.cs
@@ -6,7 +6,7 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
 
-namespace FinaktivaPT.Authentificaticon
+namespace FinaktivaPT.Authentification
 {
     public class TokenAuthenticationService: ITokenService
     {
